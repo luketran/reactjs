9ab70000f7593fbf976aad9c845155d121a1f66d
Admin <phuoctran1192@gmail.com>
2018-03-27 13:38:22 +0700

    them noi dung trang

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views","./views");
 app.listen(3000);
+
+app.get("/", function(req, res){
+    res.render("trangchu");
+});
